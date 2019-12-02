11422d29055015361cfce16cbd3c4d902aca6e52
Sei-Yukinari <sei.yukinari@nextremer.com>
2019-12-02 15:57:11 +0900

    もじ修正

--- src/logging.ts
+++ src/logging.ts
@@ -1,5 +1,6 @@
 import winston, { Logger } from 'winston';
 import encoding from 'encoding-japanese';
+import utf8 from 'utf8';
 
 const alignColorsAndTime = winston.format.combine(
     winston.format.colorize({
@@ -13,11 +14,13 @@ const alignColorsAndTime = winston.format.combine(
     }),
     winston.format.printf(
         info => {
-            console.log(encoding.detect(`${info.label}-${info.timestamp}-${info.level}:${info.message}`));
-            return `${info.label}-${info.timestamp}-${info.level}:${info.message}`;
+            console.log(encoding.detect(utf8.encode(`${info.label}-${info.timestamp}-${info.level}:${info.message}`)));
+            return utf8.encode(`${info.label}-${info.timestamp}-${info.level}:${info.message}`);
         }
     )
 );
+
+console.dir(process.env)
 
 export const logger: Logger = winston.createLogger({
     level: 'info',
